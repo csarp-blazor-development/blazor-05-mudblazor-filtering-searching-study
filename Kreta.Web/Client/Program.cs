91701c875f60ea23e9adb7ff18b1e54bda311611
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-11 15:00:50 +0100

    Regisztráljuk a szükséges szolgáltatást

--- Kreta.Web/Client/Program.cs
+++ Kreta.Web/Client/Program.cs
@@ -2,10 +2,13 @@ using Kreta.Web.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using MudBlazor.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
