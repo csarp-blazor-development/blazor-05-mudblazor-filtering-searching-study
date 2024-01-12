0236eaf8876e1d114c4b2ae4cdeefe344f89b2f6
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-12 13:23:54 +0100

    Legyen aktuális, sötét és világos palettánk

--- Kreta.Web/Client/Shared/MainLayout.razor.cs
+++ Kreta.Web/Client/Shared/MainLayout.razor.cs
@@ -1,9 +1,14 @@
+using MudBlazor;
+
 namespace Kreta.Web.Client.Shared
 {
     public partial class MainLayout
     {
         private bool _drawerOpen = true;
         private bool _isCurrentLightTheme = true;
+        private MudTheme _darkMudTheme = new MudTheme();
+        private MudTheme _lightMudTheme = new MudTheme();
+        private MudTheme _currentTheme = new MudTheme();
         void DrawerToggle()
         {
             _drawerOpen = !_drawerOpen;
