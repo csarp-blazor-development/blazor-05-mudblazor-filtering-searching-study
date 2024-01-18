bf2622d7c9dcadec39acade9d11d51d80aed57f5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-18 14:34:20 +0100

    ReloadDataAsync metódus

---
+++ Kreta.Web/Client/Pages/SchoolCitizens/StudentPage.razor.cs
@@ -0,0 +1,13 @@
+﻿using Kreta.Shared.Models;
+using MudBlazor;
+
+namespace Kreta.Web.Client.Pages.SchoolCitizens
+{
+    public partial class StudentPage
+    {
+        public async Task<TableData<Student>> ReloadDataAsync(TableState state)
+        {
+
+        }
+    }
+}
