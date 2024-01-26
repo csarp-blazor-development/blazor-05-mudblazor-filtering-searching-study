17aea6715e58d875b97efda0d05231c1cbb41e91
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-26 10:58:11 +0100

    StudentViewModel és adattagok

---
+++ Kreta.Web/Client/ViewModel/SchoolCitizens/StudentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Kreta.Web.Client.ViewModel.SchoolCitizens
+{
+    public class StudentViewModel
+    {
+        public string SerchedName { get; set; } = string.Empty;
+        public uint SearchedMinYar { get; set; } = 0;
+        public uint SearchedMaxYar { get;set; } = uint.MaxValue;
+
+    }
+}
