505b05ae15f5fc13119bfa2b9e2dab0fb0157ab1
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-27 13:16:00 +0100

    A StudentViewModel mostmár le tudja kérni az adatokat a backendről

--- Kreta.Web/Client/ViewModel/SchoolCitizens/StudentViewModel.cs
+++ Kreta.Web/Client/ViewModel/SchoolCitizens/StudentViewModel.cs
@@ -1,10 +1,30 @@
-﻿namespace Kreta.Web.Client.ViewModel.SchoolCitizens
+﻿using Kreta.HttpService.Service;
+using Kreta.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Kreta.Web.Client.ViewModel.SchoolCitizens
 {
     public class StudentViewModel : IStudenViewModel
     {
+        [Inject] public IStudentService? StudentService { get; set; }
         public string SerchedName { get; set; } = string.Empty;
         public uint SearchedMinYear { get; set; } = 0;
         public uint SearchedMaxYear { get;set; } = uint.MaxValue;
+        public async Task<TableData<Student>> InitializeAsync()
+        {
+            if (StudentService is not null)
+            {
+                List<Student> students = await StudentService.SelectAllStudent();
+                TableData<Student> data = new()
+                {
+                    Items = students,
+                    TotalItems = students.Count,
+                };
+                return data;
+            }
+            return new TableData<Student> { Items = new List<Student>() };
+        }
 
     }
 }
